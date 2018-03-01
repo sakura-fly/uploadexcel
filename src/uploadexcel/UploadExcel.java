9b7b8269fce8c0d1cc6252a2bc4e8eb0092eb5a8
sakura-fly <sourcesakura@sina.com>
2018-03-01 15:04:01 +0800

    上传excel并解析

---
+++ src/uploadexcel/UploadExcel.java
@@ -0,0 +1,46 @@
+package uploadexcel;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import parse.ParseExcel;
+
+@MultipartConfig
+@WebServlet("/upload")
+public class UploadExcel extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Part part = req.getPart("file");
+		InputStream in = part.getInputStream();
+		String header = part.getHeader("content-disposition");
+		String fileName = getFileName(header);
+		System.out.println(ParseExcel.readExcel(in, fileName));
+	}
+
+	public String getFileName(String header) {
+		/**
+		 * String[] tempArr1 = header.split(";");代码执行完之后，在不同的浏览器下，tempArr1数组里面的内容稍有区别
+		 * 火狐或者google浏览器下：tempArr1={form-data,name="file",filename="snmp4j--api.zip"}
+		 * IE浏览器下：tempArr1={form-data,name="file",filename="E:\snmp4j--api.zip"}
+		 */
+		String[] tempArr1 = header.split(";");
+		/**
+		 * 火狐或者google浏览器下：tempArr2={filename,"snmp4j--api.zip"}
+		 * IE浏览器下：tempArr2={filename,"E:\snmp4j--api.zip"}
+		 */
+		String[] tempArr2 = tempArr1[2].split("=");
+		// 获取文件名，兼容各种浏览器的写法
+		String fileName = tempArr2[1].substring(tempArr2[1].lastIndexOf("\\") + 1).replaceAll("\"", "");
+		return fileName;
+	}
+
+}
